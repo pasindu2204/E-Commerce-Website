7fe0be40ec90f442c2f15cafaf75a96a0df25299
Pasindu <chamipasindu2204@gmail.com>
2025-11-12 13:12:32 +0530

    Create Navbar

---
+++ front-end/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import Collection from './Pages/Collection.jsx'
+import Content from './Pages/Content.jsx'
+import Product from './Pages/Product.jsx';
+import Cart from './Pages/Cart.jsx';
+import Login from './Pages/Login.jsx';
+import PlaceOrder from './Pages/PlaceOrder.jsx';
+import Order from './Pages/Order.jsx';
+import Navbar from './Components/Navbar.jsx';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/content' element={<Content />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/place-order' element={<PlaceOrder />} />
+          <Route path='/order' element={<Order />} />
+
+        </Routes>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/Components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {assets} from '../assets/assets';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+
+    <div className='flex items-center justify-between py-4 font-medium '>
+        <img src={assets.logo} alt="Logo" className='w-36 h-10'/>
+
+        <ul className='hidden sm:flex items-center gap-5 text-sm text-gray-700'>
+            <NavLink to='/' className='flex flex-col items-center gap-1'>
+                <p>HOME</p>
+                <hr className='w-2/4 h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+                <p>COLLECTION</p>
+                <hr className='w-2/4 h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+
+            <NavLink to='/about' className='flex flex-col items-center gap-1'>
+                <p>ABOUT</p>
+                <hr className='w-2/4 h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+
+            <NavLink to='/content' className='flex flex-col items-center gap-1'>
+                <p>CONTENT</p>
+                <hr className='w-2/4 h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+        </ul>
+
+            {/* Search Icon */}
+        <div className="flex items-center gap-6">
+            <img src={assets.search_icon} alt="Search" className='w-5 cursor-pointer'/>
+            {/* Profile Icon */}
+
+            <div className='group relative'>
+                <img src={assets.profile_icon} alt="User" className='w-5 cursor-pointer'/>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100
+                     text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Order</p>
+                        <p className='cursor-pointer hover:text-black'>Log Out</p>
+                    </div>
+                </div>
+
+            </div>
+            {/* Cart Icon */}
+            <Link to='/cart' className='relative'>
+                <img src={assets.cart_icon} alt="Cart" className='w-5 cursor-pointer'/>
+                <p className='absolute -top-3 -right-3 w-5 h-5 rounded-full bg-black
+                 text-white aspect-square text-xs flex items-center justify-center'>10</p>
+            </Link>
+
+            {/* Dropdown Menu */}
+            <div className='group relative'>
+            <img src={assets.menu_icon} alt="Menu" className='w-5 cursor-pointer sm:hidden relative'/>
+              <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100
+                     text-gray-500 rounded'>
+                        <a href='/' className='cursor-pointer hover:text-black'>HOME</a>
+                        <a href='/collection' className='cursor-pointer hover:text-black'>COLLECTION</a>
+                        <a href='/about' className='cursor-pointer hover:text-black'>ABOUT</a>
+                        <a href='/contact' className='cursor-pointer hover:text-black'>CONTACT</a>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
