f018de90527b8ca9944b6a189217cba734ad6d3f
Pasindu <chamipasindu2204@gmail.com>
2025-11-14 11:16:17 +0530

    add policy in home page

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Components/Hero';
 import LatestCollection from '../Components/LatestCollection';
 import BestSeller from '../Components/BestSeller';
+import OuePolicy from '../Components/OuePolicy';
+import NewsLetterBox from '../Components/NewsLetterBox';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OuePolicy />
+      <NewsLetterBox />
     </div>
   )
 }
