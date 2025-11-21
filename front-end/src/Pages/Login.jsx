e6f3d9a86a19d7ebb62c795471bf29d1542cf5a4
Pasindu <chamipasindu2204@gmail.com>
2025-11-21 16:51:42 +0530

    login and sign up pages create

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+const [currentState, setCurrentState] = useState('Sign Up'); // Sign Up or Login
+
+const onSubmitHandler = async (event) => {
+    event.preventDefault();
+}
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+        <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+          <p className='prata-regular text-3xl '>{currentState}</p>
+          <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+
+        </div>
+
+       {currentState === 'Sign Up' && <input type='text' className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
+        <input type='email' className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required/>
+        {currentState === 'Sign Up' &&  <input type='password' className='w-full px-3 py-2 border border-gray-800' placeholder='New Password' required/>}
+         {currentState === 'Sign Up' && <input type='password' className='w-full px-3 py-2 border border-gray-800' placeholder='Confirm Password' required/>}
+         {currentState === 'Login' && <input type='password' className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>}
+               <div className='w-full flex justify-between text-sm mt-[-8px]'>
+                <p className='cursor-pointer'>Forgot your Password?</p>
+                {
+                  currentState === 'Login' ?
+                  <p onClick={() => setCurrentState('Sign Up')} className='cursor-pointer'>Create an account</p>
+                  :
+                  <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Login here</p>
+                }
+                </div>   
+
+                <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Login' : 'Sign Up'}</button> 
+    
+    </form>
+
+
   )
 }
 
