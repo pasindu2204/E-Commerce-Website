fdf340b48f30d762d040d48f1772efc29fb9f5d2
Pasindu <chamipasindu2204@gmail.com>
2025-11-25 14:35:22 +0530

    admin page logout button

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -42,16 +42,16 @@ const App = () => {
       <ToastContainer />
       {token === '' ? <Login setToken={setToken} /> : 
       <>
-    <Navbar />
+    <Navbar setToken={setToken} />
     <hr />
     <div className="flex w-full">
       <Sidebar />
       <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
         <Routes>
-          <Route path="/" element={ <List /> } />
-          <Route path="/add" element={ <Add /> } />
-          <Route path="/list" element={ <List /> } />
-          <Route path="/orders" element={ <Orders /> } />
+          <Route path="/" element={ <List token={token} /> } />
+          <Route path="/add" element={ <Add token={token} /> } />
+          <Route path="/list" element={ <List token={token} /> } />
+          <Route path="/orders" element={ <Orders token={token} /> } />
         </Routes>
       </div>
     </div>
