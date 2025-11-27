3e01a962415bf8c41a9f82d3157791c2c2d57c7a
Pasindu <chamipasindu2204@gmail.com>
2025-11-27 08:16:22 +0530

    admin add page coonect to DB and toastify

--- admin/src/Pages/Add.jsx
+++ admin/src/Pages/Add.jsx
@@ -2,8 +2,12 @@ import React, {useState} from 'react'
 import { assets } from '../assets/assets.js'
 import axios from 'axios'
 import {backendUrl} from '../App.jsx'
+import { toast } from 'react-toastify';
 
-const Add = () => {
+
+const Add = ({token}) => {
+
+  // states for images and form data
 
 const [image1, setImage1] = useState(false);
 const [image2, setImage2] = useState(false);
@@ -13,10 +17,12 @@ const [image4, setImage4] = useState(false);
 const [Name, setName] = useState('');
 const [Description, setDescription] = useState('');
 const [Category, setCategory] = useState('Men');
-const [Subcategory, setSubcategory] = useState('Topwear');
+const [SubCategory, setSubCategory] = useState('Topwear');
 const [Price, setPrice] = useState('');
-const [Size, setSize] = useState([]);
+const [Sizes, setSizes] = useState([]);
 const [BestSeller, setBestSeller] = useState(false);
+
+// form submit handler
 
 const onSubmitHandler = async (e) => {
   e.preventDefault(); 
@@ -29,24 +35,41 @@ const onSubmitHandler = async (e) => {
     formData.append('name', Name);
     formData.append('description', Description);
     formData.append('category', Category);
-    formData.append('subcategory', Subcategory);
+    formData.append('subCategory', SubCategory);
     formData.append('price', Price);
-    formData.append('size', JSON.stringify(Size));
+    formData.append('sizes', JSON.stringify(Sizes));
     formData.append('bestseller', BestSeller);
 
-    const response = await axios.post(
-  `${backendUrl}/api/product/add`,
-  formData,
+    // axios post request to backend
+
+    const response = await axios.post(`${backendUrl}/api/product/add`,formData,
    {
-    headers: {
-      token: localStorage.getItem("token"),
-      "Content-Type": "multipart/form-data"
-    }}
-);
-    console.log(response.data);
-    
+    headers: {token}
+  });
+
+// tost notifications
+
+    if (response.data.success) {
+      toast.success(response.data.message); 
+    setName('');
+    setDescription('');
+    setCategory('Men');
+    setSubCategory('Topwear');
+    setPrice('');
+    setSizes([]);
+    setBestSeller(false);
+    setImage1(false);
+    setImage2(false);
+    setImage3(false);
+    setImage4(false);
+
+    } else {
+      toast.error(response.data.message); 
+    }
+
   } catch (error) {
-    
+    console.log(error);
+    toast.error(error.message);
   }
 
 }
@@ -95,7 +118,7 @@ const onSubmitHandler = async (e) => {
       </div>
       <div>
         <p className='mb-2'>Product Subcategory</p>
-        <select onChange={(e) => setSubcategory(e.target.value)} className='w-full px-3 py-2'>
+        <select onChange={(e) => setSubCategory(e.target.value)} className='w-full px-3 py-2'>
           <option value="Topwear">Topwear</option>
           <option value="Bottomwear">Bottomwear</option>
           <option value="Winterwear">Winterwear</option>
@@ -110,20 +133,20 @@ const onSubmitHandler = async (e) => {
       <div>
         <p className='mb-2'>Product Size</p>
         <div className='flex gap-3'>
-        <div onClick={() =>setSize(prev => prev.includes("S") ? prev.filter(item => item !== "S") : [...prev, "S"])} >
-           <p className={`${Size.includes("S") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>S</p>
+        <div onClick={() =>setSizes(prev => prev.includes("S") ? prev.filter(item => item !== "S") : [...prev, "S"])} >
+           <p className={`${Sizes.includes("S") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>S</p>
         </div>
-        <div onClick={() =>setSize(prev => prev.includes("M") ? prev.filter(item => item !== "M") : [...prev, "M"])} >
-           <p className={`${Size.includes("M") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>M</p>
+        <div onClick={() =>setSizes(prev => prev.includes("M") ? prev.filter(item => item !== "M") : [...prev, "M"])} >
+           <p className={`${Sizes.includes("M") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>M</p>
         </div>
-        <div onClick={() =>setSize(prev => prev.includes("L") ? prev.filter(item => item !== "L") : [...prev, "L"])} >
-           <p className={`${Size.includes("L") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>L</p>
+        <div onClick={() =>setSizes(prev => prev.includes("L") ? prev.filter(item => item !== "L") : [...prev, "L"])} >
+           <p className={`${Sizes.includes("L") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>L</p>
         </div>
-        <div onClick={() =>setSize(prev => prev.includes("XL") ? prev.filter(item => item !== "XL") : [...prev, "XL"])} >
-           <p className={`${Size.includes("XL") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>XL</p>
+        <div onClick={() =>setSizes(prev => prev.includes("XL") ? prev.filter(item => item !== "XL") : [...prev, "XL"])} >
+           <p className={`${Sizes.includes("XL") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>XL</p>
         </div>
-        <div onClick={() =>setSize(prev => prev.includes("XXL") ? prev.filter(item => item !== "XXL") : [...prev, "XXL"])} >
-           <p className={`${Size.includes("XXL") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>XXL</p>
+        <div onClick={() =>setSizes(prev => prev.includes("XXL") ? prev.filter(item => item !== "XXL") : [...prev, "XXL"])} >
+           <p className={`${Sizes.includes("XXL") ? "bg-pink-300" : "bg-slate-200"} py-1 px-3 cursor-pointer`}>XXL</p>
         </div>
         </div>
       </div>
